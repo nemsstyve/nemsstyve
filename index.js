df8baf1f8211bccdee7b494dd53d853129cb7ae5
Styve NEMATCHA <92915196+nemsstyve@users.noreply.github.com>
2022-04-21 23:42:10 +0200

    Lisez-moi

---
+++ index.js
@@ -0,0 +1,37 @@
+// index.js
+const Mustache = require('mustache');
+const fs = require('fs');
+const MUSTACHE_MAIN_DIR = './main.mustache';
+/**
+  * DATA is the object that contains all
+  * the data to be provided to Mustache
+  * Notice the "name" and "date" property.
+*/
+let DATA = {
+  name: 'Styve',
+  date: new Date().toLocaleDateString('en-GB', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'Europe/Paris',
+  }),
+  heure: new Date().toLocaleDateString('en-GB', {
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZone: 'Europe/Paris',
+  }),
+};
+/**
+  * A - We open 'main.mustache'
+  * B - We ask Mustache to render our file with the data
+  * C - We create a index.md file with the generated output
+  */
+
+function generateIndexMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('index.md', output);
+  });
+}
+generateIndexMe();
