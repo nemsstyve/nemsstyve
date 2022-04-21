6618e1e5fdf78fe11aac4ad7c61251093ef73113
Styve NEMATCHA <92915196+nemsstyve@users.noreply.github.com>
2022-04-21 23:35:16 +0200

    Lisez-moi

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const MUSTACHE_MAIN_DIR = './main.mustache';
   * Notice the "name" and "date" property.
 */
 let DATA = {
-  name: 'Styve',
+  name: 'nemsstyve',
   date: new Date().toLocaleDateString('en-GB', {
     weekday: 'long',
     month: 'long',
